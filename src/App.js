6ccb638b349cfedb3b96b2f8d50375e9259db6fc
Filipe Abner <big3foot@gmail.com>
2020-07-07 23:09:32 -0300

    Estrutura basica do react

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React,{Component} from 'react';
+import './App.css';
+
+import { render } from '@testing-library/react';
+import { CardList } from "./components/card-list/card-list.component";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters:[]
+    };
+	}
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({monsters: users}))
+	}
+  render() {
+    return (
+      <div className="App">
+				<CardList  >
+      	{
+					this.state.monsters.map(monster => 
+						<h1>{monster.name}</h1>
+					)   
+       	}
+				</CardList>
+      </div>  
+    );
+  }
+}
+
+export default App;
